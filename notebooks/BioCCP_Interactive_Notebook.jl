d2591a75c10a6068f324c99d90f60ef8185b3e05
kirstvh <73718004+kirstvh@users.noreply.github.com>
2021-06-18 11:13:44 +0200

    Adjust power law

--- notebooks/BioCCP_Interactive_Notebook.jl
+++ notebooks/BioCCP_Interactive_Notebook.jl
@@ -140,8 +140,8 @@ end
 # ╔═╡ e3b4c2d8-b78c-467e-a863-5eecb8ec58dc
 begin
 	if distribution == "Zipf law"
-		md"""                                       pₘₐₓ:   $(@bind pmax_string TextField(default = "0.01")) 
-                                           pₘᵢₙ:    $(@bind              pmin_string TextField(default = "0.005")) """
+		md"""                                       pₘₐₓ/pₘᵢₙ:   $(@bind pmaxpmin_string TextField(default = "4")) 
+                                            """
 			end
 
 end
@@ -202,11 +202,10 @@ begin
 		end
 		
 		if distribution == "Zipf law"
-			pmin = parse(Float64, pmin_string)
-			pmax = parse(Float64, pmax_string)
-			α = exp(log(pmax/pmin)/(n-1))
-			C = pmax*α
-			p_vec = collect(C*α.^-(1:n))
+			ratio = parse(Float64, pmaxpmin_string)
+			α = exp(log(ratio)/(n-1))
+			p_vec = collect(α.^-(1:n))
+			p_vec = p_vec ./ sum(p_vec)
 		end
 	end
 	
@@ -508,6 +507,7 @@ if show_occ == "🔻 SHOW "
  	# module_ = 1
 # 	p = p_vec[module_]
 # 	p = maximum(p_vec) 
+	ed = Int(floor(sample_size_3*p))
 	j = 0:1:8
 			
 	x  = prob_occurence_module.(p, sample_size_3, j)
@@ -515,12 +515,13 @@ if show_occ == "🔻 SHOW "
 	
 		else
 		rank = parse(Int64, rank_string)
-			p = C*α^-rank
+		p = p_vec[rank]
 	sample_size_4 = parse(Int64, sample_size_4_string)
  	# module_ = 1
 # 	p = p_vec[module_]
 # 	p = maximum(p_vec) 
-	j = 0:1:8
+			ed = Int(floor(sample_size_4*p))
+	j = 0:1:ed*2
 			
 	x  = prob_occurence_module.(p, sample_size_4, j)
 	 plot(j,x, seriestype=[:line, :scatter], xlabel="№ occurences in sample", ylabel="probability p", title="Chance on № of occurences for specific module", size=((550,300)))	
@@ -532,11 +533,7 @@ end
 # ╔═╡ 595423df-728b-43b1-ade4-176785c54be3
 begin
 	if show_occ == "🔻 SHOW " 
-		if distribution != "Zipf law"
-			ed = floor(sample_size_3*p)
-		else
-			ed = floor(sample_size_4*p)
-		end
+
 	
 	md""" 	            ↳ `Expected times observed:`	≈ **$ed**
 		"""
